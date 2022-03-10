be13951a7c6a77a93787b9bacda4fb00f6702435
Fisher.Tai <fisher.tai@email.com>
2022-03-10 16:04:43 +0800

    remove client、新增測試相關套件

---
+++ sever/test/mock.js
@@ -0,0 +1,24 @@
+///api/user/login
+const loginTestPost = {
+  email: "kow2@emali.com",
+  password: "a12345",
+};
+///api/user/register
+const registerTestPost = {
+  username: "",
+  email: "",
+  password: "a12345",
+  role: "instructor",
+};
+///api/courese/
+const coureseTestPost = {
+  title: "How to make apple juice3",
+  description: "How to make apple juiceaaa",
+  price: "300",
+};
+
+module.exports = {
+  loginTestPost,
+  registerTestPost,
+  coureseTestPost,
+};
