d704db700ed9a73ceb181d8271898aa827bfa6d8
LAPTOP-1SGCLO1T\Fisher <unifisher02@gmail.com>
2022-04-02 21:15:05 +0800

    Test

--- sever/test/article.test.js
+++ sever/test/article.test.js
@@ -1,56 +1,56 @@
-// /* eslint-disable no-underscore-dangle */
-// const { expect } = require("chai");
-// const supertest = require("supertest");
-// const mock = require("./mock");
+/* eslint-disable no-underscore-dangle */
+const { expect } = require("chai");
+const supertest = require("supertest");
+const assert = require("assert");
+const mock = require("./mock");
 
 // require("../index");
 
-// const api = supertest("http://localhost:8080/api/user/testApi"); // 定義測試的 API 路徑
-// let APItoken; // 全域變數等待 before() 取得 Token
+const api = supertest("http://localhost:8080"); // 定義測試的 API 路徑
+let APItoken; // 全域變數等待 before() 取得 Token
 
-// // before() 測試開始前會先跑完裡面內容
-// before((done) => {
-//   api
-//     .post("/user/login") // 登入測試
-//     .set("Accept", "application/json")
-//     .send(mock.loginTestPost)
-//     .expect(200)
-//     .end((err, res) => {
-//       APItoken = res.body.token; // 登入成功取得 JWT
-//       done();
-//     });
-// });
+// before() 測試開始前會先跑完裡面內容
+before((done) => {
+  api
+    .post("/api/user/login") // 登入測試
+    .set("Accept", "application/json")
+    .send(mock.loginTestPost)
+    .expect(200)
+    .end((err, res) => {
+      APItoken = res.body.token; // 登入成功取得 JWT
+      console.log(`Test token:${APItoken}`);
+      done();
+    });
+});
 
-// // describe() 描述區塊測試內容，可視為一個測試的群組，裡面可以跑很多條測試。
-// describe("Course", () => {
-//   // it() 可撰寫每條測試內容
-//   it("Course should be an object with keys and values", (done) => {
-//     api
-//       .get("/api/courese/") // 測試取得所有文章
-//       .expect(200)
-//       .end((err, res) => {
-//         if (err) done(err);
-//         // 斷言做資料驗證
-//         expect(res.body[0]).to.have.property("_id");
-//         expect(res.body[0]._id).to.be.a("string");
-//         expect(res.body[0]).to.have.property("title");
-//         expect(res.body[0].title).to.be.a("string");
-//         expect(res.body[0]).to.have.property("price");
-//         expect(res.body[0].price).to.be.a("number");
-//         expect(res.body[0]).to.have.property("description");
-//         expect(res.body[0].description).to.be.a("string");
-//         expect(res.body[0]).to.have.property("instructor");
-//         expect(res.body[0].instructor).to.be.a("object");
-//         done();
-//       });
-//   });
+// describe() 描述區塊測試內容，可視為一個測試的群組，裡面可以跑很多條測試。
+describe("Course", () => {
+  // it() 可撰寫每條測試內容
+  // it("Course should be an object with keys and values", (done) => {
+  //   api
+  //     .get("/api/courese/") // 測試取得所有文章
+  //     .expect(200)
+  //     .end((err, res) => {
+  //       if (err) done(err);
+  //       // 斷言做資料驗證
+  //       expect(res.body[0]).to.have.property("_id");
+  //       expect(res.body[0]._id).to.be.a("string");
+  //       expect(res.body[0]).to.have.property("title");
+  //       expect(res.body[0].title).to.be.a("string");
+  //       expect(res.body[0]).to.have.property("price");
+  //       expect(res.body[0].price).to.be.a("number");
+  //       expect(res.body[0]).to.have.property("description");
+  //       expect(res.body[0].description).to.be.a("string");
+  //       expect(res.body[0]).to.have.property("instructor");
+  //       expect(res.body[0].instructor).to.be.a("object");
+  //       done();
+  //     });
+  // });
 
-//   it("should return a 200 response", (done) => {
-//     api
-//       .get("/api/courese") // 測試取得所有課程
-//       .set("Authorization", `Bearer ${APItoken}`) // 將 Bearer Token 放入 Header 中的 Authorization
-//       .expect(200, done);
-//   });
-// });
-
-// after();
+  it("should return a 200 response", (done) => {
+    api
+      .get("/api/courese") // 測試取得所有課程
+      .set("Authorization", `${APItoken}`) // 將 Bearer Token 放入 Header 中的 Authorization
+      .expect(200, done);
+  });
+});
--- sever/test/mock.js
+++ sever/test/mock.js
@@ -1,24 +1,24 @@
-// // /api/user/login
-// const loginTestPost = {
-//   email: "kow2@emali.com",
-//   password: "a12345",
-// };
-// // /api/user/register
-// const registerTestPost = {
-//   username: "",
-//   email: "",
-//   password: "a12345",
-//   role: "normal",
-// };
-// // /api/courese/
-// const coureseTestPost = {
-//   title: "How to make apple juice3",
-//   description: "How to make apple juiceaaa",
-//   price: "300",
-// };
+// /api/user/login
+const loginTestPost = {
+  email: "unifisher07@gmail.com",
+  password: "a12345",
+};
+// /api/user/register
+const registerTestPost = {
+  username: "",
+  email: "",
+  password: "a12345",
+  role: "normal",
+};
+// /api/courese/
+const coureseTestPost = {
+  title: "How to make apple juice3",
+  description: "How to make apple juiceaaa",
+  price: "300",
+};
 
-// module.exports = {
-//   loginTestPost,
-//   registerTestPost,
-//   coureseTestPost,
-// };
+module.exports = {
+  loginTestPost,
+  registerTestPost,
+  coureseTestPost,
+};
